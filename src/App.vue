35a5a822911dd1bd22d074779a1021b55c7da362
camizgon <camizgon22@hotmail.com>
2024-07-09 22:40:17 -0400

    Prueva-Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <label>Encuentra tu Super Hero</label>
+      <input id="superHero" type="number" v-model="idHero" required>
+    </div>
+    <div>
+      <button @click="setSuperHero" :disabled="heros.length > 9">Cargar Super Hero</button>
+      <!--ul>
+        <li v-for="(superHero, index) in heros" :key="index">
+          {{ superHero.name }} - {{ superHero.powerstats.power }} - {{ superHero.appearance.race }} - {{ superHero.placeofbirth.publisher }}
+        </li>
+      </ul-->
+    </div>
+    <section>
+      <CardHero
+        v-for="(hero, index) in heros"
+        :key="index"
+        :hero="hero"
+        @eliminar="eliminarHero(index)" />
+    </section>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardHero from './components/CardHero.vue';
+import axios from "axios";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CardHero
+  },
+  data() {
+    return {
+      idHero: "",
+      heros: [], // Inicializado como arreglo vacío
+      name: "",
+      power: "",
+
+    };
+  },
+  methods: {
+    async getHero(url) {
+      try {
+        const { data } = await axios.get(url);
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async setSuperHero() {
+      const url = `https://superheroapi.com/api.php/3033707663582647/${this.idHero}`;
+      console.log(url);
+      const HeroResponse = await this.getHero(url); //Espera respueta de getHero
+      console.log(HeroResponse);
+      // Verifica si HeroResponse es un objeto y lo convierte en un arreglo
+      if (HeroResponse) {
+        this.heros.push(HeroResponse);
+      }
+    },
+
+    eliminarHero(index) {
+      this.heros.splice(index, 1)
+    }
+  },
+
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+section {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  width: 80%;
+  margin-top: 2rem;
 }
 </style>
---
+++ src/components/CardHero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-hero">
+    <p>Nombre {{ hero.name }}</p>
+    <img>
+    <p>Poder</p>
+    <p>Raza</p>
+    <p>Publicado por:</p>
+    <button @click="eliminarHero">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardHero",
+  props: {
+    hero: Object
+  },
+  methods: {
+    eliminarHero() {
+      this.$emit("eliminar", this.hero);
+    }
+  }
+};
+
+
+</script>
+
+<style>
+.card-hero{
+border: 5px groove black;
+border-radius: 8px;
+padding: 1rem;
+width: 200px;
+height: 300px;
+overflow: hidden;
+}
+</style>
